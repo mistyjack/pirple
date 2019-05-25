61b2f117029af0646d50847f213acc007eb2611d
Mustafa Oladepo <oladepomustafa@gmail.com>
2019-05-25 00:20:06 +0000

    finish homework 2

---
+++ python-is-easy/homework1/main.py
@@ -0,0 +1,31 @@
+"""
+    This file contains code that prints out the 
+    characteristics of my favourite song, "number one for 
+    me" by Maher Zain who is a Swedish muslim singer.
+"""
+
+# List of song attributes begins
+song_title = "Number one for me" # String
+artist = "Maher Zain" # String
+featured_artist1 = "Mesut Kurtis" # String
+featured_artist2 = "Irfan Makki" # String
+featured_artist3 = "Raef" # String
+featured_artist4 = "Hamza Namira" # String
+album = "Forgive Me" # String
+release_year = 2012 # Integer
+genre = "R&B/Soul" # String
+duration = 4.31667 # Float
+available_on = "Deezer" # String
+# List of song attributes ends
+
+# Printing song attributes 
+print("The title of my favourite song is " + song_title)
+print("by " + artist + ".")
+print("The featured artists are " + featured_artist1 + ", "
+        + featured_artist2 + ", " + featured_artist3 + ", and "
+        + featured_artist4 + ".")
+print("It was released in the album " + album)
+print("in the year ", release_year)
+print("The genre is " + genre + ".")
+print("It lasts for ", duration, "and")
+print("it's available on " + available_on + ".")
---
+++ python-is-easy/homework2/main.py
@@ -0,0 +1,13 @@
+def artist():
+    return "Maher Zain"
+    
+def duration():
+    return 4.31667
+    
+def release_year():
+    return 2012
+    
+def boolean():
+    return True
+
+print(boolean())
