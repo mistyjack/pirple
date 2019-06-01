6a79de1b655d251282cd5122a377ff1d64f4c3ab
Mustafa Oladepo <oladepomustafa@gmail.com>
2019-06-01 11:30:19 +0000

    complete homework4

--- python-is-easy/homework4/main.py
+++ python-is-easy/homework4/main.py
@@ -1,8 +1,31 @@
+"""
+    This file contains code that adds a new item to an
+    existing list
+"""
+
+# Create the global list called myUniqueList
 myUniqueList = []
+myLeftovers = []
 
-def addListItem(li, item):
-    li.append(item)
-    return True
+# Create a function that makes it possible to add list items
+def addNewItem(item, mainList, otherList):
+    # Does item exists? no, add to global list and return true
+    if item not in mainList:
+        mainList.append(item)
+        print("Item added to unique list")
+        return True
+    
+    # Exists? add to leftover list and return false
+    else:
+        otherList.append(item)
+        print("Item added to leftover list")
+        return False
+# End of function
 
-addListItem(myUniqueList, "Hi")
-print(myUniqueList[0])
+# Test 
+addNewItem("hello", myUniqueList, myLeftovers)
+addNewItem("hi", myUniqueList, myLeftovers)
+addNewItem("hello", myUniqueList, myLeftovers)
+addNewItem("hello", myUniqueList, myLeftovers)
+addNewItem(15, myUniqueList, myLeftovers)
+print(myUniqueList)
