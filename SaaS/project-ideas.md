77e36ea362648ceb20696ffe76b4e5b4989a4517
Ubuntu <ubuntu@ip-172-31-26-35.ap-southeast-1.compute.internal>
2019-06-20 10:50:31 +0000

    initiate SaaS

---
+++ SaaS/project-ideas.md
@@ -0,0 +1,13 @@
+# Digital printing
+Allow people to upload documents with description, choose either to
+pick it up or to get it delievered and the price is generated.
+Admin should be able to manage printing operation
+
+# Delievery service
+People should be able to post when they have to be delievered and available 
+registered individuals should get it delievered (MFA)
+
+# Local market place
+Sellers in the locality will get to display their products on the platform
+(charging a commission)
+
--- python-is-easy/homework6/main.py
+++ python-is-easy/homework6/main.py
@@ -18,31 +18,61 @@
 # Determine the max width and height this terminal and screen can
 # comfortably fit without wrapping and return false if rows/cols is more
 
+def checkEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
 def drawBoard(rows, columns):
     for row in range(rows):
-        if row % 2 == 0:
-            for column in range(columns):
-                if column % 2 == 0:
-                    if column != columns - 1:
-                        print(" ", end="")
+        if  not checkEven(rows) and not checkEven(columns):
+            if row % 2 == 0:
+                for column in range(columns):
+                    if column % 2 == 0:
+                        if column != columns - 1:
+                            print(" ", end="")
+                        else:
+                            print(" ")
                     else:
-                        print(" ")
-                else:
-                    print("|", end="")
-        else:
-            for dash in range(columns):
-                if dash != columns - 1:
-                    print("-", end="")
-                else:
-                    print("-")
+                        print("|", end="")
+            else:
+                for dash in range(columns):
+                    if dash != columns - 1:
+                        print("-", end="")
+                    else:
+                        print("-")
+        elif checkEven(rows) and checkEven(columns):
+            if row % 2 == 0:
+                for column in range(1, columns+2):
+                    if column % 2 == 1:
+                        if column != columns+1:
+                            print(" ", end="")
+                        else:
+                            print(" ")
+                    else:
+                        print("|", end="")
+            else:
+                for dash in range(columns+1):
+                    if dash != columns:
+                        print("-", end="")
+                    else:
+                        print("-")
         
-drawBoard(21,41)
 
-def terminal_size():
-    import fcntl, termios, struct
-    th, tw, hp, wp = struct.unpack('HHHH',
-        fcntl.ioctl(0, termios.TIOCGWINSZ,
-        struct.pack('HHHH', 0, 0, 0, 0)))
-    return tw, th
 
-print('Number of columns and Rows: ',terminal_size())
+# def terminal_size():
+#     import fcntl, termios, struct
+#     th, tw, hp, wp = struct.unpack('HHHH',
+#         fcntl.ioctl(0, termios.TIOCGWINSZ,
+#         struct.pack('HHHH', 0, 0, 0, 0)))
+#     return th, tw
+
+# a = terminal_size()
+drawBoard(5,5)
+
+# if even, odd, excess dash line at end
+# if odd, even, not aligned as a result of end=""
+# 
+
+
