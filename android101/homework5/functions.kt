91077f05f127f8aabfc7fc5ad22d34acecd2c586
mistyjack <oladepomustafa@gmail.com>
2020-05-26 09:06:36 +0100

    Completed homework5 in android101.

---
+++ android101/homework5/functions.kt
@@ -0,0 +1,25 @@
+// List of song attributes begins
+var song_title: String = "Number one for me" //String
+var artist: String = "Maher Zain" //String
+var featured_artist1: String = "Mesut Kurtis" //String
+var featured_artist2: String = "Irfan Makki" //String
+var featured_artist3: String = "Raef" //String
+var featured_artist4: String = "Hamza Namira" //String
+var album: String = "Forgive Me" //String
+var release_year: Int = 2012; //Integer
+var genre: String = "R&B/Soul"; //String
+var duration: Float = 4.31667f; //Float
+var available_on: String = "Deezer"; //String
+// List of song attributes ends
+
+fun genre(): String {
+    return genre
+}
+
+fun artist(): String {
+    return artist
+}
+
+fun year(): Int {
+    return release_year
+}
